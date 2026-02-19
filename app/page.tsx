729c646e648fdeef8a2f62dc0f2e955027e74517
Peixotim <pedropeixotovz@gmail.com>
2026-02-19 07:52:17 -0300

    feat: Adiciona os novos components da pagina header e hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
+import Header from "@/components/header";
+import Hero from "@/components/hero";
+
 export default function Home() {
   return (
-    <></>
+
+    <main className="relative min-h-screen bg-[#000000] text-[#FFFFFF] overflow-hidden selection:bg-[#0000FF] selection:text-white">
+      
+      {/* 1. O Cabeçalho (Fixo no topo) */}
+      <Header />
+
+      {/* 2. Primeira Dobra (Hero Section) */}
+      <Hero />
+
+      {/* As próximas seções entrarão aqui, naturalmente uma debaixo da outra */}
+      
+      {/* <Servicos /> */}
+      
+      {/* <Sobre /> */}
+      
+      {/* <Contato /> */}
+
+      {/* <Footer /> */}
+      
+    </main>
   );
 }
